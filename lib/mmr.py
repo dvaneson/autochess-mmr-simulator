d2c14803f32cc11e65b07498620f21d0233f8ee1
Donovan Ellison <dvaneson@gmail.com>
2019-04-06 20:41:22 -0700

    Adding a few comments

--- lib/mmr.py
+++ lib/mmr.py
@@ -4,12 +4,14 @@ from nptyping import Array
 CONST = np.array([127, 102, 77, 51, -51, -77, -102, -127])
 COEFF = .1725
 
+# Calculate the placements for a player given the average of the lobby
 def placements(avg_mmr: int, mmr: int) -> Array[int, 8]:
     out = ((avg_mmr - mmr) * COEFF + CONST).astype(int)
     out[0] = max(out[0], 15)
     out[7] = min(out[7], -15)
     return out
 
+# Calculate the lobby average and the mmr change for every placement and every player
 def all_placements(all_mmr: Array[int, 8]) -> Array[int, 8, 8]:
     avg_mmr = all_mmr.mean()
     out = np.array([placements(avg_mmr, mmr) for mmr in all_mmr])
